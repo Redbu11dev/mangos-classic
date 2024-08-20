992c173fd1e73432d97d5f0794102621dcbca81b
Redbu11 <Redbu11dev777@gmail.com>
2024-08-20 17:44:18 +0300

    [custom] turned off default build modules

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -10,7 +10,7 @@ option(BUILD_PLAYERBOTS                     "Build Playerbots mod"              
 option(BUILD_AHBOT                          "Build Auction House Bot mod"               ON)
 
 # Modules
-option(BUILD_MODULES                        "Build module system"                       ON)
+option(BUILD_MODULES                        "Build module system"                       OFF)
 foreach(MODULE_NAME ${MODULE_NAMES})
   if(NOT ${MODULE_NAME} STREQUAL "MODULES")
     string(TOLOWER ${MODULE_NAME} LOWER_MODULE_NAME)
